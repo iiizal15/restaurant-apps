8395a2ce5886e71b090055d44d921121adac8ad5
iiizal15 <fahrizal.20113@mhs.unesa.ac.id>
2023-06-01 02:48:48 +0700

    Update Fix Jumbotron Image

--- src/scripts/components/jumbotron.js
+++ src/scripts/components/jumbotron.js
@@ -6,10 +6,13 @@ class Jumbotron extends HTMLElement {
 
   render() {
     this.innerHTML = `
-      <picture class= "banner">
+    <div class="banner">
+      <div class="banner-content"> Find a <span>Restaurant</span> According to Your <span>Stomach's Desire</span></div>
+      <picture>
         <source media="(max-width: 600px)" srcset="/images/heros/banner-small.jpg">
         <img src="/images/heros/banner-large.jpg" alt="banner">
       </picture>
+    </div>
     `;
   }
 }
