a3c54e92eb30bb46f1cf9e9ecc87dffd5560e76d
iiizal15 <fahrizal.20113@mhs.unesa.ac.id>
2023-06-01 02:42:01 +0700

    Update Jumbotron Img

--- src/scripts/components/jumbotron.js
+++ src/scripts/components/jumbotron.js
@@ -6,13 +6,10 @@ class Jumbotron extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <div class="banner">
-      <div class="banner-content"> Find a <span>Restaurant</span> According to Your <span>Stomach's Desire</span></div>
-      <picture>
+      <picture class= "banner">
         <source media="(max-width: 600px)" srcset="/images/heros/banner-small.jpg">
         <img src="/images/heros/banner-large.jpg" alt="banner">
       </picture>
-    </div>
     `;
   }
 }
